2ade31032553a74fe9182cc100b757c65867bbf3
mubarak-mustopha <mustophamubarack17@gmail.com>
2025-11-04 12:57:26 +0100

    The return value of a thread functions return can a pointer to any type 	* mythread function returns a pointer to a myret_t type

---
+++ threads/t3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <assert.h>
+#include <stdlib.h>
+
+typedef struct {
+	int a;
+	int b;
+} myarg_t;
+
+typedef struct {
+	int x;
+	int y;
+} myret_t;
+
+void *mythread(void *arg){
+	myret_t *rvals = (myret_t *) malloc(sizeof(myret_t)); assert ( rvals != NULL );
+	rvals->x = 1;
+	rvals->y = 2;
+	return (void *) rvals;
+}
+
+int main(int argc, char *argv[]){
+	pthread_t t1;
+	myarg_t args = { 10, 25 };
+	myret_t *rvals;
+	
+	assert ( pthread_create(&t1, NULL, mythread, &args) == 0 );
+	// wait till thread t1 terminates
+	assert ( pthread_join(t1, (void **) &rvals) == 0 );
+	printf("returned: %d %d\n", rvals->x, rvals->y);
+	
+	return 0;
+}
